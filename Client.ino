da5c190ac256c28d00ee88ee3ae7931db105de23
LYF <function86437@gmail.com>
2017-06-13 18:13:31 +0800

    Client side arduino

---
+++ Client.ino
@@ -0,0 +1,128 @@
+#include <ESP8266WiFi.h>
+
+#include <WiFiClient.h>
+
+//TCP client
+WiFiClient wifiClient;
+
+//config
+#define SSID "LALALA"
+#define PASSWD "12346789"
+#define TCP_IP "192.168.75.98"
+#define TCP_PORT 8888
+
+#define deviceID 1
+
+//pin config
+int in_msg = D0;
+int out_msg = D2;
+int wifi_connect = D5;
+
+//the Wifi radio's status
+int status = WL_IDLE_STATUS;
+
+char _buffer[3];
+
+//Initial
+void setup() {
+
+   pinMode(in_msg, OUTPUT);
+   pinMode(wifi_connect, OUTPUT);
+   pinMode(out_msg, INPUT_PULLUP);
+
+   Serial.begin(9600);
+   while (!Serial)
+    ;
+
+   Serial.println("Connecting...");
+   
+   // check for the presence of the shield
+   if ( WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+
+    while(true);
+   }
+
+   // Wait for connection
+   while ( WiFi.waitForConnectResult() != WL_CONNECTED) {
+    WiFi.begin(SSID, PASSWD);
+    Serial.println("Retry");
+    delay(1000);
+   }
+   
+   Serial.println("Conneted");
+   wifiClient.connect(TCP_IP,TCP_PORT);
+
+   //set client ID
+   _buffer[0] = 'C';
+
+   //first char is character, second and third are ID
+   String(deviceID, HEX).toCharArray((_buffer + 1), 2);
+    
+   //send to server
+   wifiClient.write(&_buffer[0], 2);
+
+   //clear buffer
+   for (int i = 1; i < sizeof(_buffer); i ++) {
+    _buffer[i] = {0};
+   }
+   
+   
+}
+
+static char buffer[32];
+static int i = 0;
+static unsigned long currentTimer = millis();
+void loop() {
+
+  
+  //Trigger event by pressed button for 0.2 sec
+  int first = digitalRead(out_msg);
+  delay(200);
+  int second = digitalRead(out_msg);
+   
+  if(!first){
+    if(first != second){
+      
+      //first char is ID
+      String(deviceID, HEX).toCharArray(_buffer, 2);
+
+      //event trigger
+      _buffer[1] = '1';
+      wifiClient.write(&_buffer[0], 2);
+
+      //clear buffer
+      for(int i = 1; i < sizeof(_buffer); i++){
+        _buffer[i] = {0};
+      }
+    }
+  }
+
+  //If no incoming data, .available() = -1.  
+  if (wifiClient.available() > 0) {
+    wifiClient.read((unsigned char *)buffer, 32);
+
+    if ( buffer[0] == '1') {
+      digitalWrite(in_msg, HIGH);
+      Serial.println(buffer[0]);
+    }
+    else if (buffer[0] == '0') {
+      digitalWrite(in_msg, LOW);
+    }
+
+    //clear buffer
+    for (int i = 1; i < sizeof(buffer); i++) {
+      buffer[i] = {0};
+    }
+  }
+  
+  if(millis() - currentTimer >= 10000) {
+    if(!wifiClient.connected()){
+
+      //reconnect
+      setup();
+    } else {
+      currentTimer = millis();
+    }
+  }
+}
